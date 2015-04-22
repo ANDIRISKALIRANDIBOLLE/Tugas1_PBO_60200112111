9d986a9fcdf40a0b52862dac4a1f1cee65f60c9e
ANDIRISKALIRANDIBOLLE <andiriskal21@gmail.com>
2015-04-22 15:17:57 +0800

    versi 1.0.0

---
+++ src/riskal/riskal.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package riskal;
+
+/**
+ * @author acer
+ *
+ */
+public class riskal {
+
+	/**
+	 * @param args
+	 */
+	static String pengetahuan1 = "Print";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String praktikum1 = "BIODATA";
+		System.out.println(praktikum1);
+		biodata();
+		pembagian();
+
+	}
+	
+	public static void biodata()
+	{
+		System.out.println("Riskal");
+		System.out.println("60200112111");
+		System.out.println("KelasC");
+	}
+	
+	public static void pembagian()
+	{
+		float a = 10/2;
+	    float b=10/8;
+		System.out.println("PEMBAGIAN");
+		System.out.println("Hasilnya Adalah:"+" "+a);
+		System.out.println("Hasilnya Adalah:"+" "+b);
+	}
+
+}
